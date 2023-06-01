82cc10ee8140a9f5a772f7038f5ccef4c589831f
lightlyprogramming <113968882+lightlyprogramming@users.noreply.github.com>
2023-05-31 23:31:03 -0700

    formatting and number generation

--- Board.java
+++ Board.java
@@ -1,6 +1,7 @@
 public class Board {
 
     private char grid[][];
+    private int numGrid[][];
     private int length;
     private int width;
     int bombs;
@@ -14,6 +15,8 @@ public class Board {
 
         grid = new char[x][y];
         populateGrid();
+        numGrid = new int[x][y];
+        numberGen();
     }
 
     public Board(int x, int y, int b){
@@ -25,6 +28,8 @@ public class Board {
         grid = new char[x][y];
         populateGrid();
         populateBombs(b);
+        numGrid = new int[x][y];
+        numberGen();
     }
 
     private void populateGrid(){
@@ -41,24 +46,26 @@ public class Board {
 
         for(char[] x : grid){
             for(char y : x){
-                System.out.print(y + " ");
+                System.out.print(y + "  ");
             }
-            System.out.print(i + "\n");
+            System.out.print("|  " + i + "\n");
             i++;
         }
+
+        System.out.print(spacer(length) + "\n");
 
         // first digit: l/((int)(Math.pow(10, (int) Math.log10(l)))), doesn't work if 0
         for(int l = 0; l < grid[0].length; l++){
             if(l < 10){
-                System.out.print("0 ");
+                System.out.print("0  ");
             } else{
-                System.out.print(l/((int)(Math.pow(10, (int) Math.log10(l)))) + " ");
+                System.out.print(l/((int)(Math.pow(10, (int) Math.log10(l)))) + "  ");
                 
             }
         }
         System.out.println();
         for(int l = 0; l < grid[0].length; l++){
-            System.out.print(l % 10 + " ");
+            System.out.print(l % 10 + "  ");
         }
 
         // other idea to make fool proof:
@@ -82,19 +89,66 @@ public class Board {
          
     }
 
+    public void displayNumGrid(){
+        System.out.println();
+        int i = 0;
+
+        for(int[] x : numGrid){
+            for(int y : x){
+                System.out.print(y + "  ");
+            }
+            System.out.print("|  " + i + "\n");
+            i++;
+        }
+
+        System.out.print(spacer(length) + "\n");
+
+        // first digit: l/((int)(Math.pow(10, (int) Math.log10(l)))), doesn't work if 0
+        for(int l = 0; l < grid[0].length; l++){
+            if(l < 10){
+                System.out.print("0  ");
+            } else{
+                System.out.print(l/((int)(Math.pow(10, (int) Math.log10(l)))) + "  ");
+                
+            }
+        }
+        System.out.println();
+        for(int l = 0; l < grid[0].length; l++){
+            System.out.print(l % 10 + "  ");
+        }
+    }
+
     public void populateBombs(int bombs){
-        int x, y = 0;
         for(int i = 0; i < bombs; i++){
-            x = (int) (Math.random() * length);
-            y = (int) (Math.random() * width);
+            int x = (int) (Math.random() * length);
+            int y = (int) (Math.random() * width);
             grid[x][y] = BOMB;
         }
     }
 
     private void numberGen(){
-        for(int i = 1; i < length; i++){
-            for(int l = 1; l < width; l++){
+        for(int i = 0; i < length; i++){
+            for(int l = 0; l < width; l++){
+                int adjacent = 0;
+                for(int di = (i > 0 ? -1 : 0); di <= (i < length - 1 ? 1 : 0); ++di){
+                    for(int dl = (l > 0 ? -1 : 0); dl <= (l < width - 1 ? 1 : 0); ++dl){
+                        if(di != 0 || dl != 0){
+                            if(grid[i + di][l + dl] == BOMB){
+                                adjacent++;
+                            }
+                        }
+                    }
+                }
+                /*
+                if(grid[i][l] == BOMB){
+                    numGrid[i][l] = -1;
+                } else {
+                    numGrid[i][l] = adjacent;
+                }
+                */
+                numGrid[i][l] = adjacent;
                 
+
             }
         }
 
@@ -106,6 +160,23 @@ public class Board {
         // if x or y == 0 or width/length - 1 then don't check +- 1 in that direciton
     }
 
+    public void format(int x){
+        for(int i = 0; i < x; i++){
+            System.out.println();
+        }
+    }
+
+    private String spacer(int x){
+        String space = "";
+        x *= 3;
+
+        for(int i = 0; i < x; i++){
+            space += "_";
+        }
+        return space;
+    }
     
 
 }
+
+
--- TAMV2.java
+++ TAMV2.java
@@ -1,5 +1,8 @@
 public class TAMV2 {
     public static void main(String[] args){
-        
+        Board board = new Board(16, 16, 16);
+        board.displayGrid();
+        board.format(10);
+        board.displayNumGrid();
     }
 }
